2c0c1c7b6aa6bb2db40c250d0ae503b888501655
Rainbow Asteroids <rainbow@portal2d.xyz>
2023-06-29 20:54:07 -0400

    day one complete

---
+++ bin/day_one.ml
@@ -0,0 +1,15 @@
+open Base
+open Stdio
+
+let main filename = 
+    let rec aux current max = function
+        | [] -> print_endline (Int.to_string max) 
+        | line :: lines -> 
+                if (String.equal "" line) then aux 0 (Int.max current max) lines
+                else aux ((Int.of_string line) + current) max lines
+    in 
+    aux 0 0 (In_channel.input_lines (In_channel.create filename))
+
+let () = match Array.to_list (Sys.get_argv ()) with 
+| _ :: filename :: _ -> main filename
+| _ -> print_endline "No filename specified."
